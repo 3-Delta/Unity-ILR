2c950fe7951a0c0f75a571a009de3fb9f15ef00d
kaclok <kaclok@users.noreply.github.com>
2020-08-06 00:47:50 +0800

    启动器

---
+++ Client/Assets/Scripts/Runtime/Launcher.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Launcher : MonoBehaviour
+{
+    public string dllPath = "../../Resources/Hotfix/bin/Debug/Hotfix.dll";
+    public string pdbPath = "../../Resources/Hotfix/bin/Debug/Hotfix.pdb";
+
+    private void Start()
+    {
+        string dllFullPath = Path.Combine(Application.dataPath, dllPath);
+        string pdbFullPath = Path.Combine(Application.dataPath, pdbPath);
+        
+        // 加载程序集
+        // 解析执行
+    }
+}
