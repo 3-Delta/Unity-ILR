e64cbe2782850a127fa67b7f2519d5964d31a3f9
kaclok <kaclok@users.noreply.github.com>
2020-08-05 21:02:21 +0800

    引用lib下面的一些dll

---
+++ Resources/Hotfix/Scripts/CustomVd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hotfix
+{
+    public class CustomVd
+    {
+        public GameObject gameObject;
+        public Text text;
+
+        public CustomVd()
+        {
+            
+        }
+    }
+}
