c31b2d6f68c19783a8e2594f1f1921b2b91a6fa9
kaclok <kaclok@users.noreply.github.com>
2020-08-07 01:35:49 +0800

    启动

--- Client/Assets/Scripts/Runtime/Launcher.cs
+++ Client/Assets/Scripts/Runtime/Launcher.cs
@@ -7,17 +7,9 @@ public class Launcher : MonoBehaviour
 {
     private void Start()
     {
-        string dllFullPath = ILRSettings.HotfixDllFullPath;
-        string pdbFullPath = ILRSettings.HotfixPdbFullPath;
-        
-        // 加载程序集
-        AssemblyProxy.Init();
-        AssemblyProxy.CreateStaticMethod("Hotfix.InstanceClass", "StaticFunTest", 0);
-        AssemblyProxy.CreateStaticMethod("Hotfix.InstanceClass", "StaticFunTest2", 1);
     }
 
     private void OnDestroy()
     {
-        AssemblyProxy.Clear();
     }
 }
