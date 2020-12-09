41962611eb5b5e2dea7830fdb3a49e4cf8a4cf4f
kaclok <kaclok@users.noreply.github.com>
2020-12-09 17:40:28 +0800

    ItemIdCount.cs

--- Client/Assets/Scripts/Framework/ItemIdCount.cs
+++ Client/Assets/Scripts/Framework/ItemIdCount.cs
@@ -6,14 +6,35 @@ public class ItemIdCount {
     public uint id { get; protected set; }
     public int count { get; protected set; }
 
-    public virtual bool IsEnough(int targetCount) {
-        return count >= targetCount;
+    // public virtual bool IsEnough(int targetCount) {
+    //     return count >= targetCount;
+    // }
+
+    public bool IsEnoughForBag {
+        get { return true; }
     }
 
-    public ItemIdCount() { }
+    public static bool IsEnoughInBag(IList<ItemIdCount> ls) {
+        bool ret = true;
+        if (ls != null && ls.Count > 0) {
+            for (int i = 0, length = ls.Count; i < length; ++i) {
+                ret &= (ls[i].IsEnoughForBag);
+                if (!ret) {
+                    break;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public ItemIdCount() {
+    }
+
     public ItemIdCount(uint id, int count) {
         Reset(id, count);
     }
+
     public ItemIdCount Reset(uint id, int count) {
         this.id = id;
         this.count = count;
@@ -23,15 +44,18 @@ public class ItemIdCount {
 
 public class ItemGuidCount : ItemIdCount {
     public uint guid { get; protected set; }
-    
-    public ItemGuidCount() { }
+
+    public ItemGuidCount() {
+    }
+
     public ItemGuidCount(ulong guid, uint id, int count) {
         Reset(guid, id, count);
     }
+
     public ItemGuidCount Reset(ulong guid, uint id, int count) {
         this.guid = id;
         this.Reset(id, count);
-       
+
         return this;
     }
 }
